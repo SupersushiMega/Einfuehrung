55af4c5b0d8b203dc99260d8eba066ba853662b5
SupersuhiMega <SupersushiMega@example.com>
2020-08-20 14:28:03 +0200

    BinaryCounter

--- main.c
+++ main.c
@@ -1,15 +1,27 @@
 #include <stdlib.h>
 #include <avr/io.h>
+#include <util/delay.h>
 //Test
 
 
 int main(void)
 {
-	
+	DDRD = 0xFF;
+	uint8_t test;
+	uint16_t time;
+	test = 1;
+	time = 0;
+	PORTD = test;
 	
 	while(1)
 	{ 
-		
+		test = test ++;
+		PORTD = test;
+		_delay_ms(200);
+		if (0)
+		{
+			test = 1;
+		}
 	} //end while
 }//end of main
 
