6f0f59250fedf68bde05fecbda4d6e346fbfe7a5
SupersuhiMega <SupersushiMega@example.com>
2020-08-27 11:11:55 +0200

    LCD Initialisierung Funktional

--- main.c
+++ main.c
@@ -2,14 +2,26 @@
 #include <avr/io.h>
 #include <util/delay.h>
 //Test
+
+
+
 int main(void)
 {
 	DDRD = 0xFF;
-	DDRC = (1<<PC0), (1<<PC1);
-	PORTC = (1<<PC1);
+	//DDRC = 0xFF;
+	/*PORTD = 0x00;
+	PORTC = 0xFF;*/
+	
+	DDRC |= (1<<PC0) | (1<<PC1);
+	PORTC &= ~(1<<PC1); //RS
+	PORTC |= (1<<PC0);	//E
+	//PORTC &= ~(1<<PC0);	//E
+	_delay_ms(100);
 	PORTD = 0b00001111;
-	PORTC = (0<<PC1);
-	PORTC = (1<<PC1);
+	_delay_ms(100);
+	PORTC &= ~(1<<PC0);
+	_delay_ms(100);
+	PORTC |= (1<<PC0);
 	while(1)
 	{ 
 			
