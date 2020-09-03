3796446f168e6f5d567ba6e024308397eee1479f
SupersuhiMega <SupersushiMega@example.com>
2020-09-03 09:49:23 +0200

    Mostly Functional

--- main.c
+++ main.c
@@ -3,11 +3,12 @@
 #include <util/delay.h>
 #include <string.h>
 #include "lcd.h"
-#define Signal (PINB & (1<<PC0))
+#define Signal (PINB & (1<<PB0))	//Current Status of Signal
 //Test
 
 int main(void)
 {
+	//==================================================================
 	DDRD = 0xFF;
 	PORTD = 0x00;
 	
@@ -15,32 +16,35 @@ int main(void)
 	PORTC = 0x00;
 	
 	DDRC &= (1<<PC0);
+	//==================================================================
 	
-	uint8_t before;
+	uint8_t before;	//Variable for the previous status of the signal
 	before = 0;
-	uint16_t point;
+	uint16_t point;	//Variable for checking if time should be increased
 	point = 0;
-	uint16_t time;
+	uint16_t time;	//Variable for time meassurment
 	time = 0;
-	int8_t second;
-	second = -1;
+	int8_t second;	//Variable for the current second
+	second = -1;	//set to -1 to counterakt offset
 	
-	uint8_t Min;	//Variable for Calcuation Results
+	//Variables for Decoded Numbers
+	//==================================================================
+	uint8_t Min;	//Variable for minutes
 	Min = 0;
-	uint8_t Hour;
-	Hour = 1;
-	uint8_t Day;
-	Day = 0;
-	uint8_t WeekDay;
+	uint8_t Hour;	//Variable for hours
+	Hour = 0;
+	uint8_t Day;	//Variable for days
+	Day = 1;
+	uint8_t WeekDay;//Variable for weekday
 	WeekDay = 0;
-	uint8_t Month;
+	uint8_t Month;	//Variable for month
 	Month = 0;
-	uint8_t Year;
+	uint8_t Year;	//Variable for Year
 	Year = 0;
+	//==================================================================
 	
-	lcd_init(LCD_DISP_ON_CURSOR);  // initialisieren
+	lcd_init(LCD_DISP_ON_CURSOR);  // initialize Display
 	before = Signal;
-	//lcd_puts("hello");
 	while(1)
 	{ 
 		if(Signal == before)
@@ -50,32 +54,46 @@ int main(void)
 			{
 				time++;
 				point = 0;
-			}
-			
-			if (time > 800)
-			{
-				if (Min != 0)
+				if (time > 800)
 				{
-					lcd_clrscr();
-					lcd_NOut(Hour);
-					lcd_puts(" : ");
-					lcd_NOut(Min);
-					lcd_puts("\n");
-					lcd_NOut(Day);
-					lcd_puts(".");
-					lcd_NOut(Month);
-					lcd_puts(".");
-					lcd_NOut(Year);
-					lcd_puts("   WD");
-					lcd_NOut(WeekDay);
-					Min = 0;
-					Hour = 1;
-					Day = 0;
-					WeekDay = 0;
-					Month = 0;
-					Year = 0;
+					if (time == 801)
+					{
+						//Display Update
+						//==============================================
+						lcd_clrscr();
+						if (Hour < 10) //Checking if Hour below ten to add a zero in front of the Number
+						{
+							lcd_NOut(0);
+						}
+						lcd_NOut(Hour);
+						lcd_puts(":");
+						if (Min < 10)	//Checking if Hour below ten to add a zero in front of the Number
+						{
+							lcd_NOut(0);
+						}
+						lcd_NOut(Min);
+						lcd_puts("\n");
+						lcd_NOut(Day);
+						lcd_puts(".");
+						lcd_NOut(Month);
+						lcd_puts(".");
+						lcd_NOut(Year);
+						lcd_puts("   WD");
+						lcd_NOut(WeekDay);
+						//==============================================
+						
+						//Variable reset
+						//==============================================
+						Min = 0;
+						Hour = 0;
+						Day = 1;
+						WeekDay = 0;
+						Month = 0;
+						Year = 0;
+						//==============================================
+					}
+					second = -1;
 				}
-				second = -1;
 			}
 		}
 		else
@@ -83,144 +101,144 @@ int main(void)
 			before = Signal;
 			if(Signal == 0)
 			{
-				//~ lcd_clrscr();
-				//~ lcd_NOut(time );
-				if (time < 400)
+				if (time <= 420)
 				{			
-					switch (second)	//Decoding
+					//Decoding
+					//==================================================
+					switch (second)	
 					{
+						//Minutes
+						//==============================================
 						case 21 ... 27: 
-						{
-							switch(second) //Minutes
-							{
-								case 21: Min = Min + 1; break;
-								
-								case 22: Min = Min + 2; break;
-								
-								case 23: Min = Min + 4; break;
-								
-								case 24: Min = Min + 8; break;
-								
-								case 25: Min = Min + 10; break;
-								
-								case 26: Min = Min + 20; break;
-								
-								case 27: Min = Min + 40; break;
-							}
-						}
-						
-						case 29 ... 34: 
-						{
-							switch(second)	//Hours
-							{
-								case 29: Hour = Hour + 1; break;
-								
-								case 30: Hour = Hour + 2; break;
-								
-								case 31: Hour = Hour + 4; break;
-								
-								case 32: Hour = Hour + 8; break;
-								
-								case 33: Hour = Hour + 10; break;
-								
-								case 34: Hour = Hour + 20; break;
-							}
-						}
-						
-						case 36 ... 41:	//Day
 						{
 							switch(second)
 							{
-								case 36: Day = Day + 1; break;
+								case 21: Min += 1; break;
 								
-								case 37: Day = Day + 2; break;
+								case 22: Min += 2; break;
 								
-								case 38: Day = Day + 4; break;
+								case 23: Min += 4; break;
 								
-								case 39: Day = Day + 8; break;
+								case 24: Min += 8; break;
 								
-								case 40: Day = Day + 10; break;
+								case 25: Min += 10; break;
 								
-								case 41: Day = Day + 20; break;
+								case 26: Min += 20; break;
+								
+								case 27: Min += 40; break;
 							}
+							break;
 						}
+						//==============================================
 						
-						//~ case 42 ... 44:	//Week Day
-						//~ {
-							//~ switch(second)
-							//~ {
-								//~ case 42: result[3] = result[3] + 1; break;
-								
-								//~ case 43: result[3] = result[3] + 2; break;
-								
-								//~ case 44: result[3] = result[3] + 4; break;
-							//~ }
-							//~ break;
-						//~ }
-						
-						//~ case 45 ... 49:	//Month
-						//~ {
-							//~ switch(second)
-							//~ {
-								//~ case 45: result[4] = result[4] + 1; break;
-								
-								//~ case 46: result[4] = result[4] + 2; break;
-								
-								//~ case 47: result[4] = result[4] + 4; break;
-								
-								//~ case 48: result[4] = result[4] + 8; break;
-								
-								//~ case 49: result[4] = result[4] + 10; break;
-							//~ }
-							//~ break;
-						//~ }
-						
-						//~ case 50 ... 57:	//Year
-						//~ {
-							//~ switch(second)
-							//~ {
-								//~ case 50: result[5] = result[5] + 1; break;
-								
-								//~ case 51: result[5] = result[5] + 2; break;
-								
-								//~ case 52: result[5] = result[5] + 4; break;
-								
-								//~ case 53: result[5] = result[5] + 8; break;
-								
-								//~ case 54: result[5] = result[5] + 10; break;
-								
-								//~ case 55: result[5] = result[5] + 20; break;
-								
-								//~ case 56: result[5] = result[5] + 40; break;
-								
-								//~ case 57: result[5] = result[5] + 80; break;
-							//~ }
-							//~ break;
-						//~ }
-							default:
+						//Hours
+						//==============================================
+						case 29 ... 34:
+						{
+							switch(second)	
 							{
-								break;
+								case 29: Hour += 1; break;
+								
+								case 30: Hour += 2; break;
+								
+								case 31: Hour += 4; break;
+								
+								case 32: Hour += 8; break;
+								
+								case 33: Hour += 10; break;
+								
+								case 34: Hour += 20; break;
 							}
-						}	
+							break;
+						}
+						//==============================================
+						
+						//Days
+						//==============================================
+						case 36 ... 41:
+						{
+							switch(second)
+							{
+								case 36: Day += 1; break;
+								
+								case 37: Day += 2; break;
+								
+								case 38: Day += 4; break;
+								
+								case 39: Day += 8; break;
+								
+								case 40: Day += 10; break;
+								
+								case 41: Day += 20; break;
+							}
+							break;
+						}
+						//==============================================
+						
+						//Week Day
+						//==============================================
+						case 42 ... 44:
+						{
+							switch(second)
+							{
+								case 42: WeekDay += 1; break;
+								
+								case 43: WeekDay += 2; break;
+								
+								case 44: WeekDay += 4; break;
+							}
+							break;
+						}
+						//==============================================
+						
+						//Month
+						//==============================================
+						case 45 ... 49:	
+						{
+							switch(second)
+							{
+								case 45: Month += 1; break;
+								
+								case 46: Month += 2; break;
+								
+								case 47: Month += 4; break;
+								
+								case 48: Month += 8; break;
+								
+								case 49: Month += 10; break;
+							}
+							break;
+						}
+						//==============================================
+						
+						//Year
+						//==============================================
+						case 50 ... 57:	
+						{
+							switch(second)
+							{
+								case 50: Year += 1; break;
+								
+								case 51: Year += 2; break;
+								
+								case 52: Year += 4; break;
+								
+								case 53: Year += 8; break;
+								
+								case 54: Year += 10; break;
+								
+								case 55: Year += 20; break;
+								
+								case 56: Year += 40; break;
+								
+								case 57: Year += 80; break;
+							}
+							break;
+						}
+						//==============================================
 					}
-				//~ else
-				//~ {
-					//~ number[point] = 0;
-				//~ }
-				//~ point++;
-				//~ if (point == 8)
-				//~ {
-					//~ lcd_clrscr();
-					//~ point = 0;
-					//~ for (point = 0; point < 8; point++)
-					//~ {
-						//~ lcd_NOut(number[point]);
-						//~ number[point] = 0;
-					//~ }
-					//~ point = 0;
-					//~ lcd_puts("\n");
-				//~ }
-				//~ lcd_NOut(time);
+					//==================================================	
+				}
 				time = 0;
 			}
 			else
